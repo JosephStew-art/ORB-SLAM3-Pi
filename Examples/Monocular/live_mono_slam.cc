febe924e807f31a695673ccc16274c1347ce68c0
Joseph <joseph.nomiddlename@gmail.com>
2024-09-03 11:43:16 +0200

    Added another live executable

---
+++ Examples/Monocular/live_mono_slam.cc
@@ -0,0 +1,83 @@
+#include <signal.h>
+#include <stdlib.h>
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <chrono>
+#include <opencv2/opencv.hpp>
+#include <System.h>
+
+using namespace std;
+
+bool b_continue_session;
+
+void exit_loop_handler(int s){
+   cout << "Finishing session" << endl;
+   b_continue_session = false;
+}
+
+int main(int argc, char **argv)
+{
+    if(argc != 3)
+    {
+        cerr << endl << "Usage: ./live_mono_slam path_to_vocabulary path_to_settings" << endl;
+        return 1;
+    }
+
+    // Set up signal handler
+    struct sigaction sigIntHandler;
+    sigIntHandler.sa_handler = exit_loop_handler;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+    sigaction(SIGINT, &sigIntHandler, NULL);
+    b_continue_session = true;
+
+    // Create SLAM system. It initializes all system threads and gets ready to process frames.
+    ORB_SLAM3::System SLAM(argv[1], argv[2], ORB_SLAM3::System::MONOCULAR, true);
+
+    // Initialize webcam
+    cv::VideoCapture cap(0); // Open default camera (usually the webcam)
+    if (!cap.isOpened()) {
+        cerr << "Error: Couldn't open the camera." << endl;
+        return -1;
+    }
+
+    // Set camera parameters (you may need to adjust these)
+    cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+    cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
+    cap.set(cv::CAP_PROP_FPS, 30);
+
+    cv::Mat frame;
+    int frame_count = 0;
+    auto start_time = std::chrono::steady_clock::now();
+
+    while(b_continue_session)
+    {
+        // Capture frame
+        cap >> frame;
+        if (frame.empty()) {
+            cerr << "Error: Blank frame grabbed" << endl;
+            break;
+        }
+
+        // Get timestamp (you may want to use a more precise method)
+        auto now = std::chrono::steady_clock::now();
+        auto timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(now - start_time).count() / 1000.0;
+
+        // Pass the image to the SLAM system
+        SLAM.TrackMonocular(frame, timestamp);
+
+        frame_count++;
+        if (frame_count % 30 == 0) {
+            cout << "Processed " << frame_count << " frames" << endl;
+        }
+    }
+
+    // Stop all threads
+    SLAM.Shutdown();
+
+    // Save camera trajectory
+    SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
+
+    return 0;
+}
--- Examples/Monocular/main.cc
+++ Examples/Monocular/main.cc
@@ -1,35 +1,13 @@
-/**
- * This file is part of ORB-SLAM3
- *
- * Copyright (C) 2017-2021 Carlos Campos, Richard Elvira, Juan J. Gómez Rodríguez, José M.M. Montiel and Juan D. Tardós, University of Zaragoza.
- * Copyright (C) 2014-2016 Raúl Mur-Artal, José M.M. Montiel and Juan D. Tardós, University of Zaragoza.
- *
- * ORB-SLAM3 is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * ORB-SLAM3 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
- * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with ORB-SLAM3.
- * If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/videoio/videoio.hpp>
-#include <ORB_SLAM3/System.h>
-
-#include <iostream>
-#include <opencv2/opencv.hpp>
 #include "System.h"
 
 int main(int argc, char **argv)
 {
     if (argc != 3)
     {
-        std::cerr << "Usage: ./mono_tum path_to_vocabulary path_to_settings" << std::endl;
+        std::cerr << "Usage: ./mono_webcam path_to_vocabulary path_to_settings" << std::endl;
         return 1;
     }
 
@@ -37,16 +15,23 @@ int main(int argc, char **argv)
     ORB_SLAM3::System SLAM(argv[1], argv[2], ORB_SLAM3::System::MONOCULAR, true);
 
     // Open the camera
-    cv::VideoCapture cap(0); // 0 is the id of the CSI camera, change if necessary
+    cv::VideoCapture cap(0);
     if (!cap.isOpened())
     {
         std::cerr << "Error: Could not open the camera." << std::endl;
         return 1;
     }
 
+    // Set camera properties
+    cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+    cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
+    cap.set(cv::CAP_PROP_FPS, 30);
+
     cv::Mat frame;
+    double t_frame = 0.0;
     while (true)
     {
+        // Capture frame
         cap >> frame;
         if (frame.empty())
         {
@@ -54,13 +39,22 @@ int main(int argc, char **argv)
             break;
         }
 
+        // Convert to RGB if necessary
+        cv::cvtColor(frame, frame, cv::COLOR_BGR2RGB);
+
+        // Resize frame
+        cv::resize(frame, frame, cv::Size(600, 350));
+
+        // Get timestamp
+        t_frame = cv::getTickCount() / cv::getTickFrequency();
+
         // Pass the image to the SLAM system
-        SLAM.TrackMonocular(frame, cv::getTickCount() / cv::getTickFrequency());
+        SLAM.TrackMonocular(frame, t_frame);
 
         // Display the frame
         cv::imshow("Frame", frame);
-        if (cv::waitKey(1) == 27)
-        { // Press 'Esc' to exit
+        if (cv::waitKey(30) == 27) // Wait for 30ms or until 'Esc' is pressed
+        { 
             break;
         }
     }
